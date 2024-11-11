ab55538ae6cc25dfc5e1ec4f595c252aa7b86551
Surasek Nusati <surasek@gmail.com>
2024-11-11 22:16:12 +0700

    add linter, start day 18

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@
 // import { day6 } from './day6/day6.js';
 // import { day7 } from './day7/day7.cjs';
 
-import {day8} from './day8/day8';
-import {day9} from './day9/day9';
-import {day10} from './day10/day10';
-import {day10_ai} from './day10/day10_ai';
-import {day11} from './day11/day11';
-import {day12} from './day12/day12';
-import {day13} from './day13/day13';
-import day14 from './day14/day14';
-import day15 from './day15/day15';
-import day16 from './day16/day16';
-import day17 from './day17/day17';
+// import {day8} from './day8/day8';
+// import {day9} from './day9/day9';
+// import {day10} from './day10/day10';
+// import {day10_ai} from './day10/day10_ai';
+// import {day11} from './day11/day11';
+// import {day12} from './day12/day12';
+// import {day13} from './day13/day13';
+// import day14 from './day14/day14';
+// import day15 from './day15/day15';
+// import day16 from './day16/day16';
+// import day17 from './day17/day17';
+import day18 from './day18/day18';
 
-day17();
+day18();
--- src/utils.ts
+++ src/utils.ts
@@ -2,76 +2,76 @@ import fs from 'node:fs';
 import readline from 'readline';
 
 export function readLines(fileName: string) {
-  const fileStream = fs.createReadStream(fileName);
-  return readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
+    const fileStream = fs.createReadStream(fileName);
+    return readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
 }
 
 export function spiltWith(delimiter: string, str: string) {
-  return str
-    .split(delimiter)
-    .map((item) => item.trim())
-    .filter((item) => item);
+    return str
+        .split(delimiter)
+        .map(item => item.trim())
+        .filter(item => item);
 }
 
 export function spiltWithSpace(str: string) {
-  return str
-    .split(' ')
-    .map((item) => item.trim())
-    .filter((item) => item);
+    return str
+        .split(' ')
+        .map(item => item.trim())
+        .filter(item => item);
 }
 
-
 export function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
 }
 
 export function waitKeyInput(text?: string) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-  return new Promise(resolve => rl.question(text || 'Press key to continue', ans => {
-    rl.close();
-    resolve(ans);
-  }))
+    return new Promise(resolve =>
+        rl.question(text || 'Press key to continue', ans => {
+            rl.close();
+            resolve(ans);
+        }),
+    );
 }
 
 export enum Colors {
-  Red,
-  Blue,
-  Yellow,
-  Green,
-  Magenta,
-};
-
+    Red,
+    Blue,
+    Yellow,
+    Green,
+    Magenta,
+}
 
 export function colorToConsoleCode(color: Colors): string[] {
-  let codes: number[] = [];
+    let codes: number[] = [];
 
-  if (color === Colors.Blue) {
-    codes = [94, 39];
-  } else if (color === Colors.Red) {
-    codes = [91, 39];
-  } else if (color === Colors.Yellow) {
-    codes = [93, 39];
-  } else if (color === Colors.Green) {
-    codes = [92, 39];
-  } else if (color === Colors.Magenta) {
-    codes = [95, 39];
-  } else {
-    throw new Error('Unknown color: ' + color);
-  }
+    if (color === Colors.Blue) {
+        codes = [94, 39];
+    } else if (color === Colors.Red) {
+        codes = [91, 39];
+    } else if (color === Colors.Yellow) {
+        codes = [93, 39];
+    } else if (color === Colors.Green) {
+        codes = [92, 39];
+    } else if (color === Colors.Magenta) {
+        codes = [95, 39];
+    } else {
+        throw new Error('Unknown color: ' + color);
+    }
 
-  return ['\x1b[' + codes[0] + 'm', '\x1b[' + codes[1] + 'm'];
+    return ['\x1b[' + codes[0] + 'm', '\x1b[' + codes[1] + 'm'];
 }
 
 export function getColorText(text: string, color: Colors): string {
-  let codes = colorToConsoleCode(color);
-  return codes[0] + text + codes[1] + '\x1b[0m';
+    const codes = colorToConsoleCode(color);
+    return codes[0] + text + codes[1] + '\x1b[0m';
 }
