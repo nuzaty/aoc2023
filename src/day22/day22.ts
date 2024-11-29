8b9b1276946b52c4754918d8cdff70427851ea6f
Surasek Nusati <surasek@gmail.com>
2024-11-29 22:03:30 +0700

    completed day 22 part 2

--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -26,38 +26,82 @@ export type Line = {
     end: Point2D;
 };
 
-type Range = {
-    min: number;
-    max: number;
-};
-
-type RenderData = {
-    xRange: Range;
-    yRange: Range;
-    zRange: Range;
-};
-
 export default async function () {
-    const isPart1 = true;
-
     // step 1: read input
     const bricks: Brick[] = await readPuzzleInput();
-    // console.log(bricks);
 
-    console.log('before falling');
-    const renderData = prepareRenderBrick(bricks);
-    renderBrick(bricks, renderData);
-
+    /** PART 1 **/
     // step 2: simulate brick falling & find supporting bricks
     const supportingBrick = new Map<number, number[]>();
 
     // do from the lowest brick first
+    simulateFalling(bricks, supportingBrick);
+
+    console.log('supportingBrick.size', supportingBrick.size);
+
+    // find bricks that are supporting other bricks only itself
+    const supportingOnlyItself = new Set(
+        Array.from(supportingBrick.values())
+            .filter(v => v.length === 1)
+            .flat(),
+    );
+
+    const supportingOnlyItselfCount: number = supportingOnlyItself.size;
+
+    const safelyDisintegratedCount = bricks.length - supportingOnlyItselfCount;
+    console.log(
+        '[PART 1 Answer] safelyDisintegratedCount:',
+        safelyDisintegratedCount,
+    );
+
+    console.log(
+        'supportingOnlyItselfCount',
+        supportingOnlyItselfCount,
+        'bricks.length',
+        bricks.length,
+    );
+
+    /** PART 2 **/
+    let totalFall = 0;
+    let finishedCount = 0;
+
+    for (const brickId of supportingOnlyItself) {
+        const newBricks = bricks
+            .filter(b => b.id !== brickId)
+            .map(b => {
+                return {...b, start: {...b.start}, end: {...b.end}};
+            });
+
+        totalFall += simulateFalling(newBricks, supportingBrick);
+
+        finishedCount++;
+        if (finishedCount % 10 === 0) {
+            console.log(
+                'brickId',
+                brickId,
+                'totalFall',
+                totalFall,
+                'finishedCount',
+                finishedCount,
+            );
+        }
+    }
+
+    console.log('[PART 2 Answer] totalFall:', totalFall);
+}
+
+// return the number of other bricks that would fall
+function simulateFalling(
+    bricks: Brick[],
+    supportingBrick: Map<number, number[]>,
+): number {
+    let fallenBrickCount = 0;
+
     bricks.sort((a, b) => a.minZ - b.minZ);
 
     for (const brick of bricks) {
-        // console.log(brick.label + ' is processing...', 'z before:', brick.minZ);
-
         let falling = true;
+        const oldZ = brick.minZ;
         while (falling) {
             // try to push brick down
             pushBrickDown(brick);
@@ -69,13 +113,6 @@ export default async function () {
                     b !== brick,
             );
 
-            // console.log(
-            //     'z to check overlaps count',
-            //     zBricks.length,
-            //     'at z = ',
-            //     brick.minZ,
-            // );
-
             const overlapBricks: number[] = [];
 
             for (const zBrick of zBricks) {
@@ -83,20 +120,6 @@ export default async function () {
 
                 if (isOverlaps) {
                     overlapBricks.push(zBrick.id);
-
-                    // console.log(
-                    //     brick.label + ' found overlaps with',
-                    //     zBrick.label,
-                    //     'at z =',
-                    //     brick.minZ,
-                    // );
-                } else {
-                    // console.log(
-                    //     brick.label + ' no overlaps found with',
-                    //     zBrick.label,
-                    //     'at z =',
-                    //     brick.minZ,
-                    // );
                 }
             }
 
@@ -114,82 +137,12 @@ export default async function () {
                 }
             }
         }
-
-        // console.log(brick.label + ' is processed', 'z after:', brick.minZ);
-    }
-
-    console.log('after falling');
-    const renderDataNew = prepareRenderBrick(bricks);
-    renderBrick(bricks, renderDataNew);
-
-    console.log('supportingBrick.size', supportingBrick.size);
-
-    // find bricks that are supporting other bricks only itself
-    const supportingOnlyItselfCount: number = new Set(
-        Array.from(supportingBrick.values())
-            .filter(v => v.length === 1)
-            .flat(),
-    ).size;
-
-    const safelyDisintegratedCount = bricks.length - supportingOnlyItselfCount;
-    console.log(
-        '[PART 1 Answer] safelyDisintegratedCount:',
-        safelyDisintegratedCount,
-    );
-
-    console.log(
-        'supportingOnlyItselfCount',
-        supportingOnlyItselfCount,
-        'bricks.length',
-        bricks.length,
-    );
-
-    // step 3 : find bricks that are supporting other bricks only itself
-    // for (const brick1 of bricks) {
-    //     for (const brick2 of bricks) {
-    //         if (brick1 !== brick2) {
-    //             if (isBrickOverlaps(brick2, brick1, true)) {
-    //                 console.log(
-    //                     'bug detail!',
-    //                     util.inspect(brick1, {depth: null}),
-    //                     util.inspect(brick2, {depth: null}),
-    //                 );
-    //                 throw new Error(
-    //                     'it has a bug! with ' +
-    //                         brick1.id +
-    //                         ',' +
-    //                         brick2.id +
-    //                         ' z1=' +
-    //                         brick1.minZ +
-    //                         ' z2=' +
-    //                         brick2.minZ,
-    //                 );
-    //             }
-    //         }
-    //     }
-    // }
-}
-
-function isBrickOverlaps(brick1: Brick, brick2: Brick) {
-    for (let x1 = brick1.start.x; x1 <= brick1.end.x; x1++) {
-        for (let y1 = brick1.start.y; y1 <= brick1.end.y; y1++) {
-            for (let z1 = brick1.start.z; z1 <= brick1.end.z; z1++) {
-                for (let x2 = brick2.start.x; x2 <= brick2.end.x; x2++) {
-                    for (let y2 = brick2.start.y; y2 <= brick2.end.y; y2++) {
-                        for (
-                            let z2 = brick2.start.z;
-                            z2 <= brick2.end.z;
-                            z2++
-                        ) {
-                            if (x1 === x2 && y1 === y2 && z1 === z2) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
+        if (oldZ !== brick.minZ) {
+            fallenBrickCount++;
         }
     }
+
+    return fallenBrickCount;
 }
 
 function isBrickOverlapsXY(brick1: Brick, brick2: Brick): boolean {
@@ -323,125 +276,6 @@ async function readPuzzleInput(): Promise<Brick[]> {
     }
 
     return bricks;
-}
-
-function prepareRenderBrick(bricks: Brick[]): RenderData {
-    const xRange: Range = {min: Infinity, max: -Infinity};
-    const yRange = {min: Infinity, max: -Infinity};
-    const zRange = {min: Infinity, max: -Infinity};
-
-    for (const brick of bricks) {
-        xRange.min = Math.min(brick.start.x, xRange.min);
-        xRange.max = Math.max(brick.end.x, xRange.max);
-        yRange.min = Math.min(brick.start.y, yRange.min);
-        yRange.max = Math.max(brick.end.y, yRange.max);
-        zRange.min = Math.min(brick.start.z, zRange.min);
-        zRange.max = Math.max(brick.end.z, zRange.max);
-    }
-
-    return {
-        xRange,
-        yRange,
-        zRange,
-    };
-}
-
-function renderBrick(bricks: Brick[], renderData: RenderData) {
-    // convert brick array into two 2d array
-    const xzPlane: string[][] = init2dPlane(
-        renderData.xRange.max + 1,
-        renderData.zRange.max + 1,
-    );
-
-    const yzPlane: string[][] = init2dPlane(
-        renderData.yRange.max + 1,
-        renderData.zRange.max + 1,
-    );
-
-    for (const brick of bricks) {
-        // xz plane
-        for (let z = brick.start.z; z <= brick.end.z; z++) {
-            for (let x = brick.start.x; x <= brick.end.x; x++) {
-                if (xzPlane[z][x] === '.')
-                    xzPlane[z][x] = getColorText(
-                        brick.label,
-                        Colors[brick.labelColor as keyof typeof Colors],
-                    );
-                else xzPlane[z][x] = '?';
-            }
-        }
-        // yz plane
-        for (let z = brick.start.z; z <= brick.end.z; z++) {
-            for (let y = brick.start.y; y <= brick.end.y; y++) {
-                if (yzPlane[z][y] === '.')
-                    yzPlane[z][y] = getColorText(
-                        brick.label,
-                        Colors[brick.labelColor as keyof typeof Colors],
-                    );
-                else yzPlane[z][y] = '?';
-            }
-        }
-    }
-
-    // render xz plane
-    console.log('');
-
-    // x-axis label
-    for (let x = 0; x < xzPlane[0].length; x++) {
-        if (x === Math.floor(xzPlane[0].length / 2)) {
-            process.stdout.write('x');
-        } else {
-            process.stdout.write(' ');
-        }
-    }
-    process.stdout.write('\n');
-    // x-axis number
-    for (let x = 0; x < xzPlane[0].length; x++) {
-        process.stdout.write('' + x);
-    }
-    process.stdout.write('\n');
-
-    for (let z = xzPlane.length - 1; z >= 0; z--) {
-        for (let x = 0; x < xzPlane[0].length; x++) {
-            process.stdout.write(xzPlane[z][x]);
-        }
-        process.stdout.write(' ' + z);
-        if (z === Math.floor(xzPlane.length / 2)) {
-            process.stdout.write(' z');
-        }
-        process.stdout.write('\n');
-    }
-    // render yz plane
-    console.log('');
-    // y-axis label
-    for (let y = 0; y < yzPlane[0].length; y++) {
-        if (y === Math.floor(yzPlane[0].length / 2)) {
-            process.stdout.write('y');
-        } else {
-            process.stdout.write(' ');
-        }
-    }
-    process.stdout.write('\n');
-    // y-axis number
-    for (let y = 0; y < yzPlane[0].length; y++) {
-        process.stdout.write('' + y);
-    }
-    process.stdout.write('\n');
-
-    for (let z = yzPlane.length - 1; z >= 0; z--) {
-        for (let y = 0; y < yzPlane[0].length; y++) {
-            process.stdout.write(yzPlane[z][y]);
-        }
-        process.stdout.write(' ' + z);
-        if (z === Math.floor(yzPlane.length / 2)) {
-            process.stdout.write(' z');
-        }
-        process.stdout.write('\n');
-    }
-}
-
-function init2dPlane(width: number, height: number): string[][] {
-    return new Array(height).fill('.').map(() => new Array(width).fill('.'));
 }
 
 export function isPointOnAxisAlignedLine(line: Line, point: Point2D): boolean {
